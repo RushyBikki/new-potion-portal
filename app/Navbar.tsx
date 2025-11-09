41c128b1e570c80e2d9155e297059bff9ce264df
ChrisHerkins <chrisherkinsmain@gmail.com>
2025-11-08 22:48:51 -0600

    save1nav

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,21 +1,22 @@
 import React from "react";
-
+import Link from 'next/link';
 function Navbar() {
     return (
         <header className="bg-gradient-to-r from-green-600 to-purple-600  text-white py-6">
             <div className="flex container mx-auto px-4 justify-between items-center">
-                <h1 className="text-3xl font-bold tracking-wide"> NavBar</h1>
+                <h1 className="text-3xl font-bold tracking-wide"> Poyo's Potion Portal</h1>
                 <nav>
                     <ul className="flex space-x-4 ">
                         <li className="hover:text-yellow-300 transition duration-300">
-                            <a href="#" className="font-medium">
-                                Settings
-                            </a>
+                            <Link href="/" className="font-medium">
+                                Home
+                            </Link>
                         </li>
                         <li className="hover:text-yellow-300 transition duration-300">
-                            <a href="#" className="font-medium">
-                                About
-                            </a>
+                            <Link href="/History" className="font-medium">
+                                History
+                            </Link>
+                            
                         </li>
                     </ul>
                 </nav>
